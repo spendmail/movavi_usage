fcca4dc7479f764896e7d76f776787d130090fa9
Sizikov Konstantin <spendlively@mail.ru>
2018-08-21 22:58:29 +0700

    Оформил запуск

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # movavi_usage
+
+### Example of usage spendmail/movavi library
+
+1. Install
+ + git clone https://github.com/spendmail/movavi_usage.git
+ + cd movavi_usage/
+ + composer install
+
+2. Run example
+ + cd bin/
+ + chmod a+x run
+ + ./run
---
+++ src/Example.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spendlively
+ * Date: 21.08.18
+ * Time: 22:46
+ */
+
+namespace Application;
+
+use Movavi\Builder\AverageRateBuilder;
+use Movavi\Factory\CurrencyServiceFactory;
+use Movavi\Service\CbrService;
+use Movavi\Service\RbcService;
+
+/**
+ * Class Example
+ *
+ * Example of usage spendmail/movavi library
+ *
+ * @package Application
+ */
+class Example
+{
+    /**
+     * Starts examples
+     *
+     * @throws \Movavi\Exception\EmptyRateListException
+     * @throws \Movavi\Exception\UnknownServiceException
+     * @throws \Movavi\Exception\WrongClassException
+     */
+    public function run()
+    {
+        $eol = php_sapi_name() === 'cli' ? PHP_EOL : '<br />';
+
+        $date = new \DateTime();
+
+        $serviceFactory = new CurrencyServiceFactory();
+        $averageRateBuilder = new AverageRateBuilder();
+
+        $rbcService = $serviceFactory->createService(RbcService::NAME);
+        $cbrService = $serviceFactory->createService(CbrService::NAME);
+
+        /**
+         * Getting average rates
+         * (cbr.ru is not always working!!!)
+         */
+        $usdRates = [];
+        $usdRates[] = $rbcService->getUsdToRubRate($date);
+        $usdRates[] = $cbrService->getUsdToRubRate($date);
+
+        $averageUsdRate = $averageRateBuilder->fromArray($usdRates);
+        printf("Average USD rate: %s%s", $averageUsdRate->getRate(), $eol);
+
+        $eurRates = [];
+        $eurRates[] = $rbcService->getEurToRubRate($date);
+        $eurRates[] = $cbrService->getEurToRubRate($date);
+
+        $averageUsdRate = $averageRateBuilder->fromArray($eurRates);
+        printf("Average EUR rate: %s%s", $averageUsdRate->getRate(), $eol);
+    }
+}
